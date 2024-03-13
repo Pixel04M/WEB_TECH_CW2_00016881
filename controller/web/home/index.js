7d6783973d819845e682dfc3093d7f0d7554adb6
PIxel04M <boburmaxamataliyev>
2024-03-13 23:26:11 +0500

    updated

--- controller/web/home/index.js
+++
@@ -1,8 +0,0 @@
-const home_controller = {
-        index: async (req, res) =>{
-            res.render('home');
-        }  
-    };
-      
-    module.exports = home_controller;
-    
--- routes/web/home/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const home_controller = require('../../../controllers/web/home');
-
-router.get('/', home_controller.index);
-
-module.exports = router;
-
